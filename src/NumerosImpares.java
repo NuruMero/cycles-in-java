3fc758b53974f9c635ee429fa3b31319ac5c8503
NuruMero <adanguerreromesa@gmail.com>
2025-01-23 12:17:55 +0100

    NumerosImpares

---
+++ src/NumerosImpares.java
@@ -0,0 +1,12 @@
+public class NumerosImpares {
+    public static void main(String[] args) {
+        System.out.println("*** Números impares ***");
+        var contador = 0;
+        do {
+            if (contador %2 != 0) {
+                System.out.print(contador + " ");
+            }
+            contador++;
+        } while (contador <= 20);
+    }
+}
